179d730912e5dfce1ea165a28d0cb273c1f40f0c
esteban <juangomez1947@outlook.com>
2024-08-08 15:50:31 -0500

    Logica para elegir que navegador ejecutar

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -1,5 +1,41 @@
 package utils;
 
+import net.serenitybdd.model.environment.EnvironmentSpecificConfiguration;
+import net.thucydides.model.util.EnvironmentVariables;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverManager {
-    
+    private static WebDriver driver;
+    private static EnvironmentVariables env;
+
+    public static WebDriver getBrowserDriver() {
+        if (driver == null) {
+            String driverSelected = EnvironmentSpecificConfiguration.from(env).getProperty("driver").toLowerCase();
+
+            switch (driverSelected) {
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    break;
+                case "firefox":
+                    System.setProperty("webdriver.firefox.driver", "src/test/resources/geckodriver.exe");
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    break;
+                default:
+                    throw new IllegalArgumentException("Navegador inválido: " + "'" + driverSelected + "'");
+            }
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
